a0a1c01c9f2279dcb76b7d2c817b97b79fc9a66f
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-11-08 15:49:15 +0100

    Added querying data from imdb.com

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -15,7 +15,7 @@
 	    			'tarief': [0]}
 	    		];*/
 
-angular.module('parkingApp', ['ngRoute'])
+angular.module('movieApp', ['ngRoute'])
 
 	.config(function ($routeProvider) {
 		$routeProvider
@@ -28,37 +28,24 @@ angular.module('parkingApp', ['ngRoute'])
             });;
 	})
 
-	.controller('homeCtrl', function ($scope, addressSrv, zoneSrv, saveSrv) {
+	.controller('homeCtrl', function ($scope, searchSrv, authorSrv, saveSrv) {
 
 		$('#searchButton').on('click', function (e) {
+			var author = $('#authorText').val();
+			
+			console.log(author);
 
-			$scope.color = '';
-
-			var address = $('#addressText').val();
-
-			addressSrv.getCoordinates(address).then(function (data) {
-				var lat = parseFloat(data.data[0].lat);
-				var lon = parseFloat(data.data[0].lon);
-				var zones = saveSrv.getObject('zones');
-
-				if (Object.keys(zones).length == 0) {
-					zoneSrv.getZones().then(function (data) {
-						zones = data;
-						saveSrv.setObject('zones', data);
-						$scope.color = zoneSrv.getTariff(lon, lat, zones.data);
-					});
-				}
-				else {
-					$scope.color = zoneSrv.getTariff(lon, lat, zones.data);
-				}
-			});
+			searchSrv.getAuthor(author)
+				.then(function(data) {
+					console.log(JSON.stringify(data));
+				})
 		});
 	})
 
-	.service('addressSrv', function ($http, $q) {
-		this.getCoordinates = function (address) {
+	.service('searchSrv', function ($http, $q) {
+		this.getAuthor = function (author) {
 			var q = $q.defer();
-			var url = 'http://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(address) + '&format=json';
+			var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(author);
 
 			$http.get(url)
 				.then(function (data) {
@@ -71,7 +58,7 @@ angular.module('parkingApp', ['ngRoute'])
 		};
 	})
 
-	.service('zoneSrv', function ($http, $q) {
+	.service('authorSrv', function ($http, $q) {
 		this.getZones = function () {
 			var q = $q.defer();
 			$http.get('http://datasets.antwerpen.be/v4/gis/paparkeertariefzones.json')
@@ -83,66 +70,15 @@ angular.module('parkingApp', ['ngRoute'])
 
 			return q.promise;
 		};
-
-		// http://alienryderflex.com/polygon/
-		this.inPolygon = function (location, polyLoc) {
-			var lastPoint = polyLoc[polyLoc.length - 1];
-			var isInside = false;
-			var x = location[0];
-
-			for (var i = 0; i < polyLoc.length; i++) {
-				var point = polyLoc[i];
-				var x1 = lastPoint[0];
-				var x2 = point[0];
-				var dx = x2 - x1;
-
-				if (Math.abs(dx) > 180.0) {
-					if (x > 0) {
-						while (x1 < 0)
-							x1 += 360;
-						while (x2 < 0)
-							x2 += 360;
-					}
-					else {
-						while (x1 > 0)
-							x1 -= 360;
-						while (x2 > 0)
-							x2 -= 360;
-					}
-					dx = x2 - x1;
-				}
-
-				if ((x1 <= x && x2 > x) || (x1 >= x && x2 < x)) {
-					var grad = (point[1] - lastPoint[1]) / dx;
-					var intersectAtLat = lastPoint[1] + ((x - x1) * grad);
-
-					if (intersectAtLat > location[1])
-						isInside = !isInside;
-				}
-				lastPoint = point;
-			}
-			return isInside;
-		};
-
-		this.getTariff = function (lng, lat, zones) {
-			for (var i = 0; i < zones.length; i++) {
-				var geo = JSON.parse(zones[i].geometry);
-				var coordinates = geo.coordinates[0];
-				if (this.inPolygon([lng, lat], coordinates)) {
-					return zones[i].tariefkleur;
-				}
-			}
-		};
 	})
 
 	.service('saveSrv', function ($window, $http) {
 		this.setObject = function (key, value) {
-			$window.localStorage[key] = JSON.stringify(value);
-			//Save in CouchDB
-			//$http.put('../../' + key, value);
+			$http.put('../../' + key, value);
 		};
 
 		this.getObject = function (key) {
-			return JSON.parse($window.localStorage[key] || '{}');
+			//return JSON.parse($window.localStorage[key] || '{}');
+			return JSON.parse($http.get('../../' + key));
 		};
 	});
